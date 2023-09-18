95bceef05048440fed7b3521583c7fc1cfaca86f
Mapoma Martin <mapoma1112@hotmail.com>
2023-09-18 22:34:13 +0000

    bnb

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,14 @@
 starts a Flask web application
 """
 
-from flask import Flask, render_template
+from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def index():
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def hello_hbnb():
     """returns Hello HBNB!"""
-    return render_template("10-hbnb_filters.html")
+    return "Hello HBNB!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
